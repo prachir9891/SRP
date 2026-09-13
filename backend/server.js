0063f28e3132b3522776f305110448042393e5c7
Prachi <prachi989139@gmail.com>
2026-09-13 20:00:12 +0530

    Update config for production deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 import connectDB from './config/db.js';
 import studentRoutes from './routes/studentRoutes.js';
 
@@ -15,9 +16,19 @@ app.use(express.json());
 
 app.use('/api/students', studentRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Student Registration API is running...');
-});
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('Student Registration API is running...');
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
